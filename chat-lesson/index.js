e5497cb56f22e9f63f2b5c2b5ba01561b698fd04
mafazans <arif.mafazan@gmail.com>
2017-10-27 10:11:25 +0700

    update

--- chat-lesson/index.js
+++ chat-lesson/index.js
@@ -1,15 +1,19 @@
+// declare variable
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+// check if the server running
 server.listen(3000, function(){
   console.log('Server is Running Man!')
 });
+
+// get the response
 app.get('/', function(request, response){
   response.sendFile(__dirname + '/index.html');
 });
 
-
+// if connection, tell me
 io.on('connection', function(socket){
   console.log('A connection was made.');
 })
